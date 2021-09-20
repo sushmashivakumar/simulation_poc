3494b8186a1116d7d52e6de6517126f5c443c9a6
Rajkumar <rasingh@lohika.com>
2021-09-20 10:20:09 +0530

    main simulation poc basic structure

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+// import styles from "./App.css";
+import "./App.css";
+
+export const App = ()=>{
+
+  return (
+
+    <div>
+      Simulation  POC
+
+ 
+
+    {/* Closing Body */}
+</div>
+  // </div>
+  );
+}
+
---
+++ frontend/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+import { App } from './App';
+
+render(
+
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.querySelector('#root')
+)
